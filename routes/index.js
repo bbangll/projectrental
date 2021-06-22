36a918a8f4872cbc394883180ed76ddf7c24e410
Muhammad Durrani <bbangdurrani@muhammads-mbp.home>
2021-06-23 02:08:35 +1000

    latest update

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var router = require('express').Router();
 const passport = require('passport');
-const indexCtrl = require('../controllers/index');
-
 
 // The root route renders our only view
-router.get('/', indexCtrl.index);
+router.get('/', function(req, res) {
+  res.redirect('/users')
+});
 
 // Google OAuth login route
 router.get('/auth/google', passport.authenticate(
@@ -16,8 +16,8 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/', // where do you want the client to go after you login 
-    failureRedirect : '/' // where do you want the client to go if login fails
+    successRedirect : '/users', // where do you want the client to go after you login 
+    failureRedirect : '/users' // where do you want the client to go if login fails
   }
 ));
 
